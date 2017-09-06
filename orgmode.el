78f94476f826db752c19760961cbc5738d3ca1eb
Evgenii Lepikhin <johnlepikhin@gmail.com>
2017-09-06 23:43:09 +0300

    New features

--- orgmode.el
+++ orgmode.el
@@ -1,4 +1,7 @@
 
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
@@ -7,3 +10,26 @@
 
 
 (setq org-agenda-files (list "~/.emacs.d/org/"))
+
+(add-to-list 'org-modules 'org-id)
+(add-to-list 'org-modules 'org-checklist)
+(add-to-list 'org-modules 'org-gnus)
+(add-to-list 'org-modules 'org-mouse)
+
+
+(require 'ox-latex)
+
+(setq org-latex-default-packages-alist
+      '(("AUTO" "inputenc" t ("pdflatex"))
+       ("T2A" "fontenc" t ("pdflatex"))
+       ("" "graphicx" t)
+       ("" "grffile" t)
+       ("" "longtable" nil)
+       ("" "wrapfig" nil)
+       ("" "rotating" nil)
+       ("normalem" "ulem" t)
+       ("" "amsmath" t)
+       ("" "textcomp" t)
+       ("" "amssymb" t)
+       ("" "capt-of" nil)
+       ("" "hyperref" nil)))
