66d25cd402e6a6bf84501bc6a53254d177385978
Evgenii Lepikhin <johnlepikhin@gmail.com>
2017-09-06 20:33:45 +0300

    add isearch keybindings

--- common.el
+++ common.el
@@ -65,3 +65,15 @@
 (setq column-number-mode t)
 
 (ac-config-default)
+
+
+;; isearch keybindings
+
+(define-key isearch-mode-map (kbd "<up>") 'isearch-ring-retreat )
+(define-key isearch-mode-map (kbd "<down>") 'isearch-ring-advance )
+
+(define-key isearch-mode-map (kbd "<left>") 'isearch-repeat-backward)
+(define-key isearch-mode-map (kbd "<right>") 'isearch-repeat-forward)
+
+(define-key minibuffer-local-isearch-map (kbd "<left>") 'isearch-reverse-exit-minibuffer)
+(define-key minibuffer-local-isearch-map (kbd "<right>") 'isearch-forward-exit-minibuffer)
