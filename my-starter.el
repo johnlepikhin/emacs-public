d416abe10889c3e93297113a3b1622bbe8e16bbc
Evgenii Lepikhin <johnlepikhin@gmail.com>
2017-11-01 22:29:15 +0300

    Add starters

--- my-starter.el
+++ my-starter.el
@@ -15,4 +15,17 @@
   (set-background-color "ivory1")
   (gnus))
 
+(defun start-devel ()
+  (add-to-list 'load-path "~/.emacs.d/public/devel")
+  (load-dir "~/.emacs.d/public/devel"))
+
+(defun start-desktop ()
+  (interactive)
+  (start-general)
+  (start-devel))
+
+(defun start-mobile ()
+  (interactive)
+  (start-general))
+
 (provide 'my-starter)
