c1e88e5104790fe38109abc180a9e1a86cf719de
Evgenii Lepikhin <johnlepikhin@gmail.com>
2017-11-01 21:53:12 +0300

    Make my-blogs optional

--- my-starter.el
+++ my-starter.el
@@ -1,8 +1,13 @@
 
 (require 'load-dir)
 
+(defun start-general ()
+  (interactive)
+  (load-file "~/.emacs.d/public/blog/my-blogs.el"))
+
 (defun start-gnus-n-server ()
   (interactive)
+  (start-general)
   (server-start)
   (add-to-list 'load-path "~/.emacs.d/mygnus")
   (load-dir-one "~/.emacs.d/mygnus")
