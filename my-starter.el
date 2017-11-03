85c08672d9951074184852e7da6a6412cb74e851
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2017-11-03 20:20:05 +0300

    updates

--- my-starter.el
+++ my-starter.el
@@ -1,29 +1,6 @@
 
 (require 'load-dir)
 
-(defun start-frame-title-with-prefix (prefix)
-  (interactive "Mname: ")
-  (setq-default
-   frame-title-format
-   '("adsasd"
-     (:eval
-     (format "%s@%s: %s %s"
-             (or (file-remote-p default-directory 'user)
-                 user-real-login-name)
-             (or (file-remote-p default-directory 'host)
-                 system-name)
-             (buffer-name)
-             (cond 
-              (buffer-file-truename
-               (concat "(" buffer-file-truename ")"))
-              (dired-directory
-               (concat "{" dired-directory "}"))
-              (t
-               "[no file]"))) ))))
-
-(start-frame-title-with-prefix "zzzz")
-(setq-default frame-title-format "--- %b")
-
 (defun start-general ()
   (interactive)
   (add-to-list 'load-path "~/.emacs.d/public/blog/elisp")
@@ -36,7 +13,6 @@
   (add-to-list 'load-path "~/.emacs.d/mygnus")
   (load-dir-one "~/.emacs.d/mygnus")
   (set-background-color "ivory1")
-  (start-frame-title-with-prefix "GNUS")
   (gnus))
 
 (defun start-devel ()
