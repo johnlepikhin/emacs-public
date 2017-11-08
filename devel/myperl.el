b6685853dcf9d8989b776fc6eadf8d2de04782dd
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2017-11-08 18:40:06 +0300

    updates

--- devel/myperl.el
+++ devel/myperl.el
@@ -96,14 +96,65 @@
               (progn
                 (word-search-forward "sub")
                 (perl-insert-sub-documentation-template)))))))))
-          
+
+(defun my-load-perlysense ()
+  (interactive)
+  (local-unset-key "\C-o")
+  (setq ps/key-prefix "\C-o")
+  (require 'flymake-perlcritic)
+  (setq ps/load-flymake t)
+  ;; *** PerlySense load (don't touch) ***
+  (setq ps/external-dir (shell-command-to-string "perly_sense external_dir"))
+  (if (string-match "Devel.PerlySense.external" ps/external-dir)
+      (progn
+        (message
+         "PerlySense elisp files  at (%s) according to perly_sense, loading..."
+         ps/external-dir)
+        (setq-local load-path (cons
+                               (expand-file-name
+                                (format "%s/%s" ps/external-dir "emacs")
+                                ) load-path))
+        (load "perly-sense")
+        )
+    (message "Could not identify PerlySense install dir.
+    Is Devel::PerlySense installed properly?
+    Does 'perly_sense external_dir' give you a proper directory? (%s)" ps/external-dir)
+    )
+
+
+  ;; ** Flymake Config **
+  ;; If you only want syntax check whenever you save, not continously
+  (setq flymake-no-changes-timeout 9999)
+  (setq flymake-start-syntax-check-on-newline nil)
+
+  ;; ** Code Coverage Visualization **
+  ;; If you have a Devel::CoverX::Covered database handy and want to
+  ;; display the sub coverage in the source, set this to t
+  (setq ps/enable-test-coverage-visualization nil)
+
+  ;; ** Color Config **
+  ;; Emacs named colors: http://www.geocities.com/kensanata/colors.html
+  ;; The following colors work fine with a white X11
+  ;; background. They may not look that great on a console with the
+  ;; default color scheme.
+  (set-face-background 'flymake-errline "antique white")
+  (set-face-background 'flymake-warnline "lavender")
+  (set-face-background 'dropdown-list-face "lightgrey")
+  (set-face-background 'dropdown-list-selection-face "grey")
+
+
+  ;; ** Misc Config **
+
+  ;; Run calls to perly_sense as a prepared shell command. Experimental
+  ;; optimization, please try it out.
+  (setq ps/use-prepare-shell-command t))
+
 (add-hook
  'cperl-mode-hook
  (lambda ()
    (progn
      (setq-local cperl-indent-level 4)
-     (local-unset-key "\C-o")
-     (setq-local ps/key-prefix "\C-o")
+     (my-load-perlysense)
 
      (add-hook 'before-save-hook #'perl-mode-perltidy-buffer t)
      (local-set-key (kbd "M-;") 'hippie-expand)
@@ -116,63 +167,6 @@
      (local-set-key (kbd "C-x C-M-d") 'perl-document-current-function)
      (local-set-key (kbd "C-x C-M-s") 'perl-insert-sub-template))))
 
-(require 'flymake-perlcritic)
 
-(setq ps/load-flymake t)
-
-;; *** PerlySense Config ***
-
-;; ** PerlySense **
-;; The PerlySense prefix key (unset only if needed, like for \C-o)
-
-(setq ps/load-flymake t)
-
-;; *** PerlySense load (don't touch) ***
-(setq ps/external-dir (shell-command-to-string "perly_sense external_dir"))
-(if (string-match "Devel.PerlySense.external" ps/external-dir)
-    (progn
-      (message
-       "PerlySense elisp files  at (%s) according to perly_sense, loading..."
-       ps/external-dir)
-      (setq load-path (cons
-                       (expand-file-name
-                        (format "%s/%s" ps/external-dir "emacs")
-                        ) load-path))
-      (load "perly-sense")
-      )
-  (message "Could not identify PerlySense install dir.
-    Is Devel::PerlySense installed properly?
-    Does 'perly_sense external_dir' give you a proper directory? (%s)" ps/external-dir)
-  )
-
-
-;; ** Flymake Config **
-;; If you only want syntax check whenever you save, not continously
-(setq flymake-no-changes-timeout 9999)
-(setq flymake-start-syntax-check-on-newline nil)
-
-;; ** Code Coverage Visualization **
-;; If you have a Devel::CoverX::Covered database handy and want to
-;; display the sub coverage in the source, set this to t
-(setq ps/enable-test-coverage-visualization nil)
-
-;; ** Color Config **
-;; Emacs named colors: http://www.geocities.com/kensanata/colors.html
-;; The following colors work fine with a white X11
-;; background. They may not look that great on a console with the
-;; default color scheme.
-(set-face-background 'flymake-errline "antique white")
-(set-face-background 'flymake-warnline "lavender")
-(set-face-background 'dropdown-list-face "lightgrey")
-(set-face-background 'dropdown-list-selection-face "grey")
-
-
-;; ** Misc Config **
-
-;; Run calls to perly_sense as a prepared shell command. Experimental
-;; optimization, please try it out.
-(setq ps/use-prepare-shell-command t)
-
-;; *** PerlySense End ***
 
 (provide 'myperl)
