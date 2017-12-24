a281a070a593c5318e4d2844781920112ae82187
Evgenii Lepikhin <johnlepikhin@gmail.com>
2017-12-24 17:10:06 +0300

    home notebook updates

--- my-sport.el
+++ my-sport.el
@@ -3,25 +3,21 @@
 
 (defun sport/equipment-report-weights (src)
   "Return summary weights grouped by buggage type"
-  (cons
-   (cons "ИТОГО" (nthcdr 2 (first src)))
-   (let
-       ((vals
-         (cons
-          'hline
-          (list (let ((cols (- (length (first src)) 2)))
-                  (seq-reduce '(lambda (sums row)
-                                 (mapcar* '(lambda (v s)
-                                             (+ (* (number-or-v v 0) (lst-number-or-v row 1 0))
-                                                s))
-                                          (nthcdr 2 row) sums))
-                              (cdr src)
-                              (make-list cols 0)))))))
-     (cons
-      (seq-reduce #'+ vals 0)
-      vals))))
-
-(sport/equipment-report-weights src)
+  (let
+      ((vals
+        (let ((cols (- (length (first src)) 2)))
+                (seq-reduce '(lambda (sums row)
+                               (mapcar*
+                                '(lambda (v s)
+                                   (+ (* (number-or-v v 0) (lst-number-or-v row 1 0)) s))
+                                (nthcdr 2 row)
+                                sums))
+                            (cdr src)
+                            (make-list cols 0)))))
+    (list
+     (cons "ИТОГО" (nthcdr 2 (first src)))
+     'hline
+     (cons (seq-reduce #'+ vals 0) vals))))
 
 (defun sport/equipment-report-baggage (src pos)
   "Return list for specified buggage type (column position in src table)"
