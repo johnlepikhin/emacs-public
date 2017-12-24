74909bcc7a893fadc1ca873bd7a87ad4b28bcb20
Evgenii Lepikhin <johnlepikhin@gmail.com>
2017-12-24 17:00:05 +0300

    home notebook updates

--- my-sport.el
+++ my-sport.el
@@ -4,17 +4,22 @@
 (defun sport/equipment-report-weights (src)
   "Return summary weights grouped by buggage type"
   (cons
-   (nthcdr 0 (first src))
-   (cons
-    'hline
-    (list (let ((cols (- (length (first src)) 2)))
-    (seq-reduce '(lambda (sums row)
-        (mapcar* '(lambda (v s)
-            (+ (* (number-or-v v 0) (lst-number-or-v row 1 0))
-            s))
-         (nthcdr 2 row) sums))
-   (cdr src)
-   (make-list cols 0)))))))
+   (cons "ИТОГО" (nthcdr 2 (first src)))
+   (let
+       ((vals
+         (cons
+          'hline
+          (list (let ((cols (- (length (first src)) 2)))
+                  (seq-reduce '(lambda (sums row)
+                                 (mapcar* '(lambda (v s)
+                                             (+ (* (number-or-v v 0) (lst-number-or-v row 1 0))
+                                                s))
+                                          (nthcdr 2 row) sums))
+                              (cdr src)
+                              (make-list cols 0)))))))
+     (cons
+      (seq-reduce #'+ vals 0)
+      vals))))
 
 (sport/equipment-report-weights src)
 
