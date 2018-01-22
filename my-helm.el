f4b65013377c4fc84b0a5a84aef4c9abb4aee6a8
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-01-22 15:10:05 +0300

    updates

--- my-helm.el
+++ my-helm.el
@@ -16,28 +16,30 @@
   (setq helm-google-suggest-use-curl-p t)
   (setq helm-net-prefer-curl t))
 
-(setq helm-wikipedia-suggest-url "https://ru.wikipedia.org/w/api.php?action=opensearch&search=%s"
-      helm-wikipedia-summary-url "https://ru.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=%s"
-      helm-search-suggest-action-wikipedia-url "https://ru.wikipedia.org/wiki/Special:Search?search=%s")
+(setq helm-wikipedia-suggest-url "https://en.wikipedia.org/w/api.php?action=opensearch&search=%s"
+      helm-wikipedia-summary-url "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=%s"
+      helm-search-suggest-action-wikipedia-url "https://en.wikipedia.org/wiki/Special:Search?search=%s")
 
 (setq helm-input-idle-delay 0.1)
 
-(defun my-helm-search-all ()
-  (interactive)
-  (require 'helm-x-files)
-  (let ((sources '(helm-source-buffers-list
+(setq-local my-helm-sources
+            '(helm-source-buffers-list
                    helm-source-recentf
                    helm-source-info-pages
                    helm-source-notmuch
                    helm-source-bbdb
                    helm-source-wikipedia-suggest
-                   helm-source-google-suggest)))
-    (let ((sources (append (helm-org-rifle-get-sources-for-open-buffers) sources)))
-      (unless helm-source-buffers-list
-        (setq helm-source-buffers-list
-              (helm-make-source "Buffers" 'helm-source-buffers)))
-      (helm :sources sources
-            :buffer "*helm completions*"))))
+                   helm-source-google-suggest))
+
+(defun my-helm-search-all ()
+  (interactive)
+  (require 'helm-x-files)
+  (let ((sources (append (helm-org-rifle-get-sources-for-open-buffers) my-helm-sources)))
+    (unless helm-source-buffers-list
+      (setq helm-source-buffers-list
+            (helm-make-source "Buffers" 'helm-source-buffers)))
+    (helm :sources sources
+          :buffer "*helm completions*")))
 
 (global-set-key "\M-s\ \M-s" 'my-helm-search-all)
 
