51890cc57ff98298360b9eb42c8e5fb32cf0568f
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-01-22 13:10:07 +0300

    updates

--- my-helm.el
+++ my-helm.el
@@ -17,11 +17,16 @@
 
 (setq helm-wikipedia-suggest-url "https://ru.wikipedia.org/w/api.php?action=opensearch&search=%s"
       helm-wikipedia-summary-url "https://ru.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=%s"
-      helm-search-suggest-action-wikipedia-url "https://en.wikipedia.org/wiki/Special:Search?search=%s")
+      helm-search-suggest-action-wikipedia-url "https://ru.wikipedia.org/wiki/Special:Search?search=%s")
 
 (defun my-helm-search-all ()
   (interactive)
+  (require 'helm-x-files)
+  (unless helm-source-buffers-list
+    (setq helm-source-buffers-list
+          (helm-make-source "Buffers" 'helm-source-buffers)))
   (helm :sources '(helm-source-buffers-list
+                   helm-source-recentf
                    helm-source-recentf
                    helm-source-info-pages
                    helm-source-notmuch
