2edfe8095e337193d5c783d7dc2cc4430a980216
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-01-22 12:30:08 +0300

    updates

--- my-helm.el
+++ my-helm.el
@@ -1,4 +1,6 @@
 
+(require 'helm-for-files)
+(require 'helm-notmuch)
 (require 'recentf)
 
 (recentf-mode 1)
@@ -7,12 +9,12 @@
 (defun my-helm-search-all ()
   (interactive)
   (helm :sources '(helm-source-buffers-list
-                   helm-source-recentf
                    helm-source-info-pages
                    helm-source-notmuch
                    helm-source-info-bbdb
-                   helm-source-google-suggest)
-        :buffer "*helm my command*"))
+                   helm-source-google-suggest
+                   helm-source-regexp)
+        :buffer "*helm completions*"))
 
 (global-set-key "\M-s\ \M-s" 'my-helm-search-all)
 
