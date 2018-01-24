a5097e6d79c6f7a849ef3fbc14655249c0d6a8e9
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-01-24 17:00:05 +0300

    updates

---
+++ devel/my-tags.el
@@ -0,0 +1,10 @@
+
+(setq path-to-ctags "~/bin/ctags")
+
+(defun my-create-tags (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (shell-command
+   (format "%s -f TAGS -e -R %s" path-to-ctags (directory-file-name dir-name))))
+
+(provide 'my-tags)
