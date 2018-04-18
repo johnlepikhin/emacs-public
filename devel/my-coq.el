9073d5486f915644eb7f4d95a2ed7b1a1f947636
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-04-18 16:10:01 +0300

    mail.ru laptop updates

--- devel/my-coq.el
+++ devel/my-coq.el
@@ -1,7 +1,14 @@
 
 (load "~/.emacs.d/lisp/PG/generic/proof-site")
 
-(add-hook 'coq-mode-hook #'company-coq-mode)
+(defun my-configure-coq-mode ()
+  (interactive)
+  (company-coq-mode)
+  (setq-local company-minimum-prefix-length 2)
+  (coq-double-hit-enable)
+  (auto-complete-mode -1))
+
+(add-hook 'coq-mode-hook #'my-configure-coq-mode)
 
 (setq coq-prog-name "coqtop")
 
