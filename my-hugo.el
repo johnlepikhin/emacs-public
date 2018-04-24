09762953d131653df6b35ad53613dfdbce09f3a6
Evgenii Lepikhin <johnlepikhin@gmail.com>
2018-04-24 23:40:06 +0300

    home notebook updates

--- my-hugo.el
+++ my-hugo.el
@@ -17,4 +17,34 @@
         (directory-files "~/org/personal" t "^blog-.*\\.org$")))
 
 
+;; Hyphenation
+
+(defun my-hyphenize-russian (input hyphen)
+  (interactive)
+  (with-temp-buffer
+    (progn
+      (insert input)
+      (call-process-region
+       (point-min)
+       (point-max)
+       "~/bin/hyphen/russian/russian-hyphen.pl"
+       t
+       t
+       nil
+       "--hyphenize-stdin"
+       (concat "--hyphen-char=" hyphen))
+      (buffer-string))))
+
+(org-export-define-derived-backend 'my-hugo 'hugo
+  :translate-alist '((plain-text . my-hugo-improvements)
+                     (bold . my-hugo-improvements)
+                     (italic . my-hugo-improvements)
+                     (paragraph . my-hugo-improvements)))
+
+(defun my-hugo-improvements (contents info)
+  (my-blog-macro-expand
+   (my-hyphenize-russian contents "&#173;")))
+
+
+
 (provide 'my-hugo)
