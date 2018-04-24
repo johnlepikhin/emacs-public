10f78c8edbdba2cf6925dd486878cb568ad90c8a
Evgenii Lepikhin <johnlepikhin@gmail.com>
2018-04-24 23:20:07 +0300

    home notebook updates

--- my-hugo.el
+++ my-hugo.el
@@ -5,14 +5,16 @@
   (require 'ox-hugo))
 
 (defun my-org-hugo-export-file (f)
+  (interactive)
   (save-excursion 
     (find-file f)
     (org-hugo-export-wim-to-md :all-subtrees)
     (kill-buffer (current-buffer))))
 
 (defun my-org-hugo-export-files-org-personal () 
+  (interactive)
   (mapc 'my-org-hugo-export-file
-        (directory-files dir t ".txt$")))
+        (directory-files "~/org/personal" t "^blog-.*\\.org$")))
 
 
 (provide 'my-hugo)
