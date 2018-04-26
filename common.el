ec95317443b48cf89d2944f711a6d1c29673ee35
Evgenii Lepikhin <johnlepikhin@gmail.com>
2018-04-26 08:10:07 +0300

    home notebook updates

--- common.el
+++ common.el
@@ -98,35 +98,35 @@
 
 
 ;; hydra in rectangle mode
-(require 'hydra)
-(require 'rect)
-(defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
-                           :color pink
-                           :post (deactivate-mark))
-  "
-  ^_k_^     _d_elete    _s_tring
-_h_   _l_   _o_k        _y_ank
-  ^_j_^     _n_ew-copy  _r_eset
-^^^^        _e_xchange  _u_ndo
-^^^^        ^ ^         _p_aste
-"
-  ("h" rectangle-backward-char nil)
-  ("l" rectangle-forward-char nil)
-  ("k" rectangle-previous-line nil)
-  ("j" rectangle-next-line nil)
-  ("e" hydra-ex-point-mark nil)
-  ("n" copy-rectangle-as-kill nil)
-  ("d" delete-rectangle nil)
-  ("r" (if (region-active-p)
-           (deactivate-mark)
-         (rectangle-mark-mode 1)) nil)
-  ("y" yank-rectangle nil)
-  ("u" undo nil)
-  ("s" string-rectangle nil)
-  ("p" kill-rectangle nil)
-  ("o" nil nil))
+; (require 'hydra)
+; (require 'rect)
+; (defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
+;                            :color pink
+;                            :post (deactivate-mark))
+;   "
+;   ^_k_^     _d_elete    _s_tring
+; _h_   _l_   _o_k        _y_ank
+;   ^_j_^     _n_ew-copy  _r_eset
+; ^^^^        _e_xchange  _u_ndo
+; ^^^^        ^ ^         _p_aste
+; "
+;   ("h" rectangle-backward-char nil)
+;   ("l" rectangle-forward-char nil)
+;   ("k" rectangle-previous-line nil)
+;   ("j" rectangle-next-line nil)
+;   ("e" hydra-ex-point-mark nil)
+;   ("n" copy-rectangle-as-kill nil)
+;   ("d" delete-rectangle nil)
+;   ("r" (if (region-active-p)
+;            (deactivate-mark)
+;          (rectangle-mark-mode 1)) nil)
+;   ("y" yank-rectangle nil)
+;   ("u" undo nil)
+;   ("s" string-rectangle nil)
+;   ("p" kill-rectangle nil)
+;   ("o" nil nil))
 
-(global-set-key (kbd "C-x SPC") 'hydra-rectangle/body)
+; (global-set-key (kbd "C-x SPC") 'hydra-rectangle/body)
 
 
 ;; hydra in dired
