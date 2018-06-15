fb4e07c17ed268339607cca8eb3c9921e5713bf8
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-06-15 16:20:01 +0300

    mail.ru laptop updates

--- my-undo-tree.el
+++ my-undo-tree.el
@@ -1,4 +1,14 @@
 
 (require 'undo-tree)
 
-(global-undo-tree-mode)
+(global-undo-tree-mode 1)
+
+(global-set-key (kbd "C-x u") 'undo-tree-visualize)
+
+(defalias 'undo 'undo-tree-undo)
+(global-set-key (kbd "C-z") 'undo)
+
+(defalias 'redo 'undo-tree-redo)
+(global-set-key (kbd "C-S-z") 'redo)
+
+(provide 'my-undo-tree)
