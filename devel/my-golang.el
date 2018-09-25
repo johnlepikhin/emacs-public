e7e0fc2110e251af18a9b3c43d3587dbb9e08eba
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-09-25 13:30:03 +0300

    mail.ru

--- devel/my-golang.el
+++ devel/my-golang.el
@@ -4,6 +4,7 @@
 (ac-config-default)
 
 (setq ac-go-gocode-bin "~/work/go/bin/gocode"
-      godef-command "~/work/go/bin/godef")
+      godef-command "~/work/go/bin/godef"
+      gofmt-command "~/bin/go/bin/gofmt")
 
 (provide 'my-golang)
