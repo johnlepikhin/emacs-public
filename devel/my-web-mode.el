adbc4d385ff679ffd3b9526767795766be118d87
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-11-23 22:40:03 +0300

    mail.ru

--- devel/my-web-mode.el
+++ devel/my-web-mode.el
@@ -1,6 +1,7 @@
 
 (require 'web-mode)
 
-(setq web-mode-enable-engine-detection 't)
+(setq web-mode-enable-engine-detection 't
+      web-mode-enable-auto-indentation 'nil)
 
 (provide 'my-web-mode)
