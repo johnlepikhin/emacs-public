bb4aac53d29c1af2d7bfe4ab9e1a4af4e89f7692
Evgenii Lepikhin <johnlepikhin@gmail.com>
2018-12-28 00:50:05 +0300

    home notebook updates

--- my-orgmode.el
+++ my-orgmode.el
@@ -25,6 +25,7 @@
 (setq org-bbdb-anniversary-field 'birthday)
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org\\.src$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cc" 'org-capture)
