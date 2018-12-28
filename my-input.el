a9b9b562e2c30298a2293b152f7d63e0e4e90cee
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-12-28 14:10:03 +0300

    mail.ru

--- my-input.el
+++ my-input.el
@@ -1,8 +1,13 @@
 
-; (defun my-update-cursor
-;     (if buffer-read-only "grey"
+(defun my-update-cursor ()
+  (set-cursor-color
+   (if buffer-read-only "grey"
+     (if (string= current-input-method "russian-computer")
+         "red"
+       "black"))))
 
-      
+(add-hook 'buffer-list-update-hook 'my-update-cursor)
+
 (defun my-update-isearch-input-method ()
   (if isearch-mode
       (progn
@@ -11,22 +16,12 @@
         (isearch-update))))
 
 (defun my-update-input-method (is-ru)
+  (start-process "" nil "xkblayout-state" "set" "0")
   (if is-ru
-      (progn
-        (start-process "" nil "xkblayout-state" "set" "0")
-        (set-input-method 'russian-computer)
-        ;; (set-face-attribute 'mode-line nil :background "red")
-        (set-cursor-color "red")
-        (my-update-isearch-input-method)
-        )
-    (progn
-      ;; (deactivate-input-method)
-      (inactivate-input-method)
-      (start-process "" nil "xkblayout-state" "set" "0")
-      ;; (set-face-attribute 'mode-line nil :background "light gray")
-      (set-cursor-color "black")
-      (my-update-isearch-input-method)
-      )))
+      (set-input-method 'russian-computer)
+    (inactivate-input-method))
+  (my-update-isearch-input-method)
+  (my-update-cursor))
 
 (defun my-select-input-eng ()
   (interactive)
