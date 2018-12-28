d890fddf8c6eb323c04f6efb444fac9d7f29b30b
Evgenii Lepikhin <johnlepikhin@gmail.com>
2018-12-28 23:40:03 +0300

    mail.ru

--- my-input.el
+++ my-input.el
@@ -30,6 +30,10 @@
   (interactive)
   (my-update-input-method t))
 
+(global-set-key (kbd "<f31>") 'my-select-input-eng)
+(global-set-key (kbd "<f32>") 'my-select-input-rus)
+(define-key isearch-mode-map (kbd "<f31>") 'my-select-input-rus)
+(define-key isearch-mode-map (kbd "<f32>") 'my-select-input-eng)
 ; (global-set-key (kbd "<f31>") 'my-select-input-eng)
 ; (global-set-key (kbd "<f32>") 'my-select-input-rus)
 ; (define-key isearch-mode-map (kbd "<f31>") 'my-select-input-rus)
