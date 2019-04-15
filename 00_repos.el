a33f0b48dcfa65da238a7f82930c01dd9b857d44
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-04-15 16:10:01 +0300

    changes from e-lepikhin-mail-ru on Mon Apr 15 16:10:01 MSK 2019

--- 00_repos.el
+++ 00_repos.el
@@ -1,5 +1,4 @@
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
