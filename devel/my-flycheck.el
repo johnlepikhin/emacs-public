d8de3cd565ce929af5205b2df2abb3027a8748fe
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-04-15 16:50:01 +0300

    changes from e-lepikhin-mail-ru on Mon Apr 15 16:50:01 MSK 2019

---
+++ devel/my-flycheck.el
@@ -0,0 +1,11 @@
+;;; my-flycheck --- My flycheck configuration
+
+;;; Commentary:
+
+;;; Code:
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(provide 'my-flycheck)
+
+;;; my-flycheck ends here
