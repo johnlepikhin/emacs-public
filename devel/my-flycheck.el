e92df4c449e8cb9ce76da0f98ff2a2d9c5e2b1e5
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-04-16 15:20:02 +0300

    changes from e-lepikhin-mail-ru on Tue Apr 16 15:20:01 MSK 2019

--- devel/my-flycheck.el
+++ devel/my-flycheck.el
@@ -5,10 +5,15 @@
 ;;; Code:
 
 (require 'flycheck)
+(require 'flycheck-inline)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-(setq flycheck-display-errors-function nil)
+(with-eval-after-load 'flycheck
+  (add-hook 'flycheck-mode-hook #'flycheck-inline-mode))
+
+(setq flycheck-display-errors-function nil
+      flycheck-display-errors-delay 0.1)
 
 (provide 'my-flycheck)
 
