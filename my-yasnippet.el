0bae249b6b170b0200b19fbd10ed3961b342cc12
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-04-22 16:20:01 +0300

    changes from e-lepikhin-mail-ru on Mon Apr 22 16:20:01 MSK 2019

---
+++ my-yasnippet.el
@@ -0,0 +1,14 @@
+;;; my-yasnippet --- My YASnippet configuration
+
+(require 'yasnippet)
+
+;;; Commentary:
+
+;;; Code:
+
+(setq yas-snippet-dirs '("/home/evgenii/.emacs.d/public/yasnippets"))
+
+(yas-global-mode 1)
+
+(provide 'my-yasnippet)
+;;; my-yasnippet ends here
