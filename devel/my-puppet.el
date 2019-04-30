1148261ec4c3242ba69eb6da8bab749494d3090f
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-04-30 18:20:01 +0300

    changes from e-lepikhin-mail-ru on Tue Apr 30 18:20:01 MSK 2019

--- devel/my-puppet.el
+++ devel/my-puppet.el
@@ -1,8 +1,8 @@
 
 (defun my-puppet-setup ()
   (interactive)
-  (setq puppet-indent-level 2
-        puppet-include-indent 2))
+  (setq puppet-indent-level 4
+        puppet-include-indent 4))
 
 (add-hook 'puppet-mode-hook 'my-puppet-setup)
 
