dd33b9362a3bdb0a67bf1b148cbbd6c638a560a1
Evgenii Lepikhin <johnlepikhin@gmail.com>
2019-05-12 16:40:05 +0300

    home notebook updates

--- my-yasnippet.el
+++ my-yasnippet.el
@@ -6,7 +6,7 @@
 
 ;;; Code:
 
-(setq yas-snippet-dirs '("/home/evgenii/.emacs.d/public/yasnippets"))
+(setq yas-snippet-dirs (list (expand-file-name "~/.emacs.d/public/yasnippets")))
 
 (yas-global-mode 1)
 
