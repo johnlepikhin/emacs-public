3c5dee376784b09628029635a9b2d42d01e29083
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-05-28 17:30:01 +0300

    changes from e-lepikhin-mail-ru on Tue May 28 17:30:01 MSK 2019

--- my-utils.el
+++ my-utils.el
@@ -4,11 +4,11 @@
 (setq debug-on-error 't)
 
 (defun number-or-v (n v)
-  "Return V if N is not a number, or n otherwise."
+  "Return V if N is not a number, or N otherwise."
   (if (not (numberp n)) v n))
 
 (defun lst-number-or-v (lst pos v)
-  "Return v if list element at pos is not a number, or (nth pos lst) otherwise"
+  "Return V if list element at POS is not a number, or (nth POS LST) otherwise."
   (number-or-v (nth pos lst) v))
   
 (provide 'my-utils)
