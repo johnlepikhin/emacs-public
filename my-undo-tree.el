16e17806c3b12acc81f200c506349a9b13a6a8d5
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-05-28 18:20:01 +0300

    changes from e-lepikhin-mail-ru on Tue May 28 18:20:01 MSK 2019

--- my-undo-tree.el
+++ my-undo-tree.el
@@ -5,10 +5,10 @@
 
 (global-set-key (kbd "C-x u") 'undo-tree-visualize)
 
-; (defalias 'undo 'undo-tree-undo)
-; (global-set-key (kbd "C-z") 'undo)
+(defalias 'undo 'undo-tree-undo)
+(global-set-key (kbd "C-z") 'undo)
 
-; (defalias 'redo 'undo-tree-redo)
-; (global-set-key (kbd "C-S-z") 'redo)
+(defalias 'redo 'undo-tree-redo)
+(global-set-key (kbd "C-S-z") 'redo)
 
 (provide 'my-undo-tree)
