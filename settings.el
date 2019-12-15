03e1e7ebaa31b366a8e2788265e1813f15d0ebf0
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-12-15 14:20:01 +0300

    changes from mailbook on Sun 15 Dec 2019 02:20:01 PM MSK

--- settings.el
+++ settings.el
@@ -1573,3 +1573,11 @@ This command does not push text to `kill-ring'."
   :after (calendar)
   :config
   (setq calendar-holidays russian-holidays))
+
+(defun number-or-v (n v)
+  "Return V if N is not a number, or N otherwise."
+  (if (not (numberp n)) v n))
+
+(defun lst-number-or-v (lst pos v)
+  "Return V if list element at POS is not a number, or (nth POS LST) otherwise."
+  (number-or-v (nth pos lst) v))
