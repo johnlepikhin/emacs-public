e3adc34f51c10fc09ff369c36d1c77cd3d9b64b2
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2020-02-21 22:20:01 +0300

    changes from mailbook on Fri 21 Feb 2020 10:20:01 PM MSK

--- settings.el
+++ settings.el
@@ -516,24 +516,25 @@ This command does not push text to `kill-ring'."
 (use-package go-guru
   :commands (go-guru-hl-identifier-mode))
 
-(defun my-rust-buffer-setup ()
-  (message "test")
-  (add-hook 'before-save-hook 'rust-format-buffer nil 'local)
-  (set (make-local-variable 'compile-command)
-       (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
-           "cargo run"
-         (format "rustc %s && %s" (buffer-file-name)
-                 (file-name-sans-extension (buffer-file-name))))))
-
 (use-package rust-mode
   :after (flycheck tramp racer compile)
-  :hook (rust-mode . my-rust-buffer-setup)
-  :mode ("\\.rs\\'" . rust-mode)
+  ;; :hook (rust-mode . my-rust-buffer-setup)
+  :mode "\\.rs\\'"
   :bind (:map rust-mode-map
-              ("C-c i b" . rust-format-buffer)))
-  ;; :config
-  ;; (add-hook 'rust-mode-hook (lambda () (add-hook 'before-save-hook 'rust-format-buffer nil 'local))))
-  ;; (add-hook 'before-save-hook 'rust-format-buffer))
+              ("C-c i b" . rust-format-buffer))
+  :config
+  (defun my-rust-buffer-setup ()
+    (message "test")
+    (add-hook 'before-save-hook 'rust-format-buffer nil 'local)
+    (set (make-local-variable 'compile-command)
+         (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
+             "cargo run"
+           (format "rustc %s && %s" (buffer-file-name)
+                   (file-name-sans-extension (buffer-file-name))))))
+  (add-hook 'rust-mode-hook 'my-rust-buffer-setup))
+;; :config
+;; (add-hook 'rust-mode-hook (lambda () (add-hook 'before-save-hook 'rust-format-buffer nil 'local))))
+;; (add-hook 'before-save-hook 'rust-format-buffer))
 
 (use-package racer
   :hook (rust-mode . racer-activate)
